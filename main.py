5d230b43992ca7817f43bd82af48716e1b89ea28
M Rifki Muchtar <91528096+thea725@users.noreply.github.com>
2025-12-15 00:29:00 +0700

    [0.0.1] - CTRL + Q

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import sys
 import threading
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget
-from PyQt6.QtGui import QPixmap
-from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QPixmap, QShortcut, QKeySequence
+from PyQt6.QtCore import Qt, pyqtSignal
 from pynput import keyboard
 
+VK_Q = 0x51
 
-class FloatingImageWindow(QWidget):
+class BongoWindow(QWidget):
+    
+    quitRequested = pyqtSignal()
+
     def __init__(self):
         super().__init__()
 
@@ -28,6 +32,10 @@ class FloatingImageWindow(QWidget):
         self.drag_pos = None
         self.start_keyboard_listener()
 
+        self.quitRequested.connect(QApplication.quit)
+
+        self.keys = set()
+        
     def start_keyboard_listener(self):
         t = threading.Thread(target=self._keyboard_listener_thread, daemon=True)
         t.start()
@@ -40,9 +48,13 @@ class FloatingImageWindow(QWidget):
             listener.join()
 
     def on_key_press(self, key):
+        self.keys.add(key)
+        if self.is_ctrl_q():
+            self.quitRequested.emit()
         self.set_typing(True)
 
     def on_key_release(self, key):
+        self.keys.discard(key)
         self.set_typing(False)
 
     def set_typing(self, typing):
@@ -52,6 +64,14 @@ class FloatingImageWindow(QWidget):
         else:
             self.label.setPixmap(self.img_untyping)
             self.resize(self.img_untyping.size())
+
+    def is_ctrl_q(self):
+        ctrl = keyboard.Key.ctrl_l in self.keys or keyboard.Key.ctrl_r in self.keys
+        q = any(
+            isinstance(k, keyboard.KeyCode) and k.vk == VK_Q
+            for k in self.keys
+        )
+        return ctrl and q
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
@@ -68,6 +88,6 @@ class FloatingImageWindow(QWidget):
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    w = FloatingImageWindow()
+    w = BongoWindow()
     w.show()
     sys.exit(app.exec())
